2f0f7d31ff1aed2d75f0f9034609643fe6941d10
a4a881d4 <a4a881d4@163.com>
2018-10-28 18:00:21 +0800

    msc

--- rustcode/pearls/msc/src/main.rs
+++ rustcode/pearls/msc/src/main.rs
@@ -1,6 +1,16 @@
 mod normal {
 	pub fn msc(t:&String) -> usize {
-		&*t.to_vec().len()
+		let vt = t.clone().into_bytes();
+		println!("{:?}",vt.iter().filter(|x| **x < 'g' as u8).count());
+		println!("tail[u8] = {:?}",tail(vt[..]));
+		1
+	}
+	fn tail(xs : &[u8]) -> &[u8] {
+		match xs.len() {
+			0 => [],
+			1 => [],
+			_ => xs[1..],
+		}
 	}
 }
 fn main() {
